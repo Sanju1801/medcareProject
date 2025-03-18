1d2103e2249ec9330e686825cc816aa973f207fb
Sanju <sanju.sr0018@gmail.com>
2025-03-18 15:09:39 +0530

    feature: add home page

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,20 +1,25 @@
 import Footer from "../components/Footer";
+import styles from "../styles/header.module.css";
+import Link from "next/link";
 
-export default function Booking() {
-    return (
-        <>
-        <div className="booking-container">
-            <div className="title-container">
-                <div className="title">
+export default function Home() {
+  return (
+    <div>
+        <div className={`${styles.mainContainer} ${styles.bookingContainer}`}>
+            {/* Left Section */}
+            <div className={styles.leftContainer}>
+                <div className={styles.titleContainer}>
                     <h1>Book Your Next Doctor Visit in Seconds.</h1>
                     <p>CareMate helps you find the best healthcare provider by speciality, location, and more, ensuring you get the care you need.</p>
                 </div>
-                <div className="slot-container">
-                    
-                </div> 
-            </div>  
+            </div>
+
+            {/* Right Section */}
+            <div className={`${styles.rightContainer} ${styles.booking}`}>
+            </div>
+
         </div>
         <Footer />
-        </>
-    )
-}
+    </div>
+  );
+}            
